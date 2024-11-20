16843e6def1efb45d11683e712259f58aee8af59
Sefger <132765477+Sefger@users.noreply.github.com>
2024-11-20 16:42:25 +0300

    Task1_59.cpp

--- Task1_59.cpp
+++ Task1_59.cpp
@@ -1,17 +1,17 @@
 ﻿
-
+//Составить программу обмена значениями двух переменных величин. 
 #include <iostream>
-static void Task1_59(int *a, int *b) {
-    int t = *a;
-    *a = *b;
-    *b = t;
+static void Task1_59(int &a, int &b) {
+    int t = a;
+    a = b;
+    b = t;
 }
 int main()
 {
     int a = 10;
     int b = 16;
     std::cout <<"a = " << a << " " << "b = " << b << std::endl;
-    Task1_59(&a, &b);
+    Task1_59(a, b);
     std::cout << "a = " << a << " " << "b = " << b << std::endl;
     return 0;
 }
