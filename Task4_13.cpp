2f7972c3e2941157fc44a30b57894a281a52e9d5
Sefger <132765477+Sefger@users.noreply.github.com>
2024-11-20 23:00:23 +0300

    Update Task4_13.cpp

--- Task4_13.cpp
+++ Task4_13.cpp
@@ -1,17 +1,19 @@
-﻿
 
-#include <iostream>
-#include <vector>;
 /*
 Даны вещественные числа a, b, c (a != 0). Выяснить, имеет ли уравнение 
 вещественные корни.
  */
+
+
+#include <iostream>
+#include <sstream>
+#include <vector>;
 static std::vector<double> Task4_13(double a, double b, double c) {
     
     double D = (b * b - 4 * a * c);
 
     std::vector<double> result = {};
-    if (D < 0) {
+    if (D < 0  || a==0) {
 
         return result;
     }
@@ -24,15 +26,33 @@ static std::vector<double> Task4_13(double a, double b, double c) {
 }
 int main()
 {
-    double a = 1;
-    double b = -5;
-    double c = 6;
+    std::string aString, bString, cString;
+    std::cout << "Enter a: ";
+    std::cin >> aString;
+    double a = stoi(aString);
+
+
+    std::cout << "Enter b: ";
+    std::cin >> bString;
+    double b = stoi(bString);
+
+
+    std::cout << "Enter c: ";
+    std::cin >> cString;
+    double c = stoi(cString);
+
     std::vector<double> result = Task4_13(a, b, c);
-    std::cout << "The root of equation: ";
-    for (int i = 0; i < result.size(); i++) {
-        std::cout << result[i] << " ";
+    if (result.size() == 0) {
+        std::cout << "Not equation."<<std::endl;
+    }
+    else {
+        std::cout << "The root of equation: ";
+        for (int i = 0; i < result.size(); i++) {
+            std::cout << result[i] << " ";
+        }
     }
     std::cout << std::endl;
     return 0;
 }
+
 
