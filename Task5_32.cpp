6bce40864b4d9fa329c4e7eee8dff1646f6dc0a6
Sefger <132765477+Sefger@users.noreply.github.com>
2024-11-20 22:46:55 +0300

    Update Task5_32.cpp

--- Task5_32.cpp
+++ Task5_32.cpp
@@ -1,5 +1,8 @@
-﻿#include <iostream>
-
+#include <iostream>
+#include <stdexcept>
+#include <sstream>
+/*
+    Вычислить сумму 1 + 1/2 + 1/3 + 1/4 ... 1/n .*/
 double Task5_32(int n) {
     double result = 1;
     for (double i = 2;i <= n; i++) {
@@ -8,11 +11,11 @@ double Task5_32(int n) {
     }
     return result;
 }
-int main()
+//я не понял как сделать ввод из теминала 
+int main(int argc, char* argv)
 {
-    int n = 5000;
+    int n = argv[1];
     double result = Task5_32(n);
     std::cout << "Result is: " << result <<std::endl;
     return 0;
 }
-
